6016ddc3d037c781e7489d96c51561c9d2e1953c
asu1610 <51980449+asu1610@users.noreply.github.com>
2019-07-10 10:08:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,14 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
-    sendText(user,userText)
+    #sendText(user,userText)
+     if (userText == 'สวัสดี') :
+      sendText(user,'ดีจ้ะ')
+    elif (userText == 'ไปแหละ') :
+       sendText(user,'เอ่อ! ไปเหอะ')
+    else :
+       sendText(user,'หมายควายว่าไง')
+
     return '',200
 
 def sendText(user, text):
