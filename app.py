a5c9cbedcea1bf3924ae44c44e29293acfb883b2
asu1610 <51980449+asu1610@users.noreply.github.com>
2019-10-09 11:39:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,9 +21,9 @@ def callback():
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
     user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
-    userText = decoded['queryResult']['intent']['displayName']
+    userText = decoded['queryResult']['intent']['displayName'] 
     #sendText(user,userText)
-     if (userText == 'สวัสดี') :
+    if (userText == 'สวัสดี') :
       sendText(user,'ดีจ้ะ')
     elif (userText == 'ไปแหละ') :
        sendText(user,'เอ่อ! ไปเหอะ')
